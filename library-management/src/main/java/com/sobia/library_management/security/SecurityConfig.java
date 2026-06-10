869e9ac0fe8362bc1be6ef645fdbe6c3db7e636e
sobiabashir <sobiabashir_786@hotmail.com>
2026-06-10 15:50:02 +0200

    Add Vercel URL to CORS allowed origins

--- library-management/src/main/java/com/sobia/library_management/security/SecurityConfig.java
+++ library-management/src/main/java/com/sobia/library_management/security/SecurityConfig.java
@@ -61,7 +61,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of(
+                "http://localhost:3000",
+                "https://library-management-frontend-git-main-sobia-s-projects.vercel.app"
+        ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
